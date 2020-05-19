9721334cbee07a12d1f034bb273b51e549ce0348
Geir Inge Jensen <gij@jk.priv.no>
2020-05-19 16:20:35 +0200

    Support High Spamassassin score

--- common/usr/share/MailScanner/perl/MailScanner/ConfigDefs.pl
+++ common/usr/share/MailScanner/perl/MailScanner/ConfigDefs.pl
@@ -191,6 +191,7 @@ satimeoutlen			= spamassassintimeoutshistory
 removeheaders			= removetheseheaders
 replacetnef			= usetnefcontents
 reqspamassassinscore		= requiredspamassassinscore
+highspamassassinscore           = highspamassassinscore
 sacache				= spamassassincachedatabasefile
 saviwatchfiles                  = monitorsforsophosupdates
 scanmail			= scanmessages
